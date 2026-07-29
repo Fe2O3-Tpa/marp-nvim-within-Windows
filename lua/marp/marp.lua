f5ac3971c3438e290318d92dfe6a9bbafc77c2da
Fe2O3-Tpa <gogottu1221@gmail.com>
2026-07-29 18:41:03 +0900

    Change to OS-independent code

--- lua/marp/marp.lua
+++ lua/marp/marp.lua
@@ -5,7 +5,7 @@ local M = {}
 M.jobid = 0
 
 local function marp_running()
-  return M.jobid ~= 0
+    return M.jobid ~= 0
 end
 
 --[[
@@ -17,11 +17,11 @@ end
     ```
 ]]
 function M.toggle()
-  if marp_running() then
-    M.stop()
-  else
-    M.start()
-  end
+    if marp_running() then
+        M.stop()
+    else
+        M.start()
+    end
 end
 
 --[[
@@ -33,33 +33,35 @@ end
     ```
 ]]
 function M.start()
-  if not util.dir_contains_md_files(vim.fn.getcwd()) then
-    util.log_info("no Markdown files found, exiting!")
-    return
-  end
+    if not util.dir_contains_md_files(vim.fn.getcwd()) then
+        util.log_info("no Markdown files found, exiting!")
+        return
+    end
 
-  if marp_running() then
-    util.log_info("already running")
-    return
-  end
+    if marp_running() then
+        util.log_info("already running")
+        return
+    end
 
-  local port = config.options.port
-  local wait_for_response_timeout = config.options.wait_for_response_timeout
-  local wait_for_response_delay = config.options.wait_for_response_delay
+    local port = config.options.port
+    local wait_for_response_timeout = config.options.wait_for_response_timeout
+    local wait_for_response_delay = config.options.wait_for_response_delay
 
-  local marp_start_command = "PORT=" .. port .. " marp --server " .. vim.fn.getcwd()
+    M.jobid = vim.fn.jobstart(
+        { "marp", "--server", vim.fn.getcwd() },
+        {
+            env = {
+                PORT = tostring(port),
+            },
+            on_exit = function(_, exit_code, _)
+                M.jobid = 0
+                util.log_info("exit (code=" .. exit_code .. ")")
+            end,
+        }
+    )
 
-  util.log_info("starting server on http://localhost:" .. port)
-
-  M.jobid = vim.fn.jobstart(marp_start_command, {
-    on_exit = function(_, exit_code, _)
-      M.jobid = 0
-      util.log_info("exit (code=" .. exit_code .. ")")
-    end,
-  })
-
-  util.wait_for_response("http://localhost:" .. port, wait_for_response_timeout, wait_for_response_delay)
-  util.open_url_in_browser("http://localhost:" .. port)
+    util.wait_for_response("http://localhost:" .. port, wait_for_response_timeout, wait_for_response_delay)
+    util.open_url_in_browser("http://localhost:" .. port)
 end
 
 --[[
@@ -71,9 +73,9 @@ end
     ```
 ]]
 function M.stop()
-  vim.fn.jobstop(M.jobid)
-  M.jobid = 0
-  util.log_info("server stopped")
+    vim.fn.jobstop(M.jobid)
+    M.jobid = 0
+    util.log_info("server stopped")
 end
 
 --[[
@@ -85,11 +87,11 @@ end
     ```
 ]]
 function M.status()
-  if M.jobid == 0 then
-    util.log_info("not running")
-  else
-    util.log_info("running")
-  end
+    if M.jobid == 0 then
+        util.log_info("not running")
+    else
+        util.log_info("running")
+    end
 end
 
 return M
